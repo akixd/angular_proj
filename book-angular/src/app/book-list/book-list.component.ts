de67d1f5e11b4598ed3981191b32bf84e444b811
Norbert <114875832+akixd@users.noreply.github.com>
2025-06-13 19:12:08 +0200

    wip actors

---
+++ book-angular/src/app/book-list/book-list.component.ts
@@ -0,0 +1,40 @@
+import {Component} from '@angular/core';
+import {NgForOf} from '@angular/common';
+import {ApiService} from '../api.service';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-book-list',
+  standalone: true,
+  imports: [
+    NgForOf,
+    RouterLink
+  ],
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.css'
+})
+export class BookListComponent {
+  books: any[] = [];
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.apiService.getBooks().subscribe((data: any[]) => {
+      this.books = data;
+      console.log("Books loaded:", this.books);
+    });
+  }
+
+  deleteBook(id: number) {
+    const confirmed = window.confirm(`Are you sure you want to delete ?`);
+    if (confirmed) {
+      this.apiService.deleteBook(id).subscribe({
+        next: (res) => {
+          this.books = this.books.filter(book => book.id !== id);
+        }
+      });
+
+    }
+  }
+}
---
+++ src/main/java/pl/merito/demo/Book.java
@@ -0,0 +1,72 @@
+package pl.merito.demo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String title;
+    private String author;
+    private String description;
+
+    @ManyToOne
+    private Category category;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @ManyToMany
+    private List<Actors> actors;
+
+    public List<Actors> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actors> actors) {
+        this.actors = actors;
+    }
+}
